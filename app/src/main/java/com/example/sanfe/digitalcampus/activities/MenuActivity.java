bf0bc90a11f6b74ed5e4a76f674e1e7fccde63a1
Roger Marrugat <ls31252@salleurl.edu>
2016-05-10 11:04:18 +0200

    Afegida SubjectActivity (In Progress) i ActionBar

--- app/src/main/java/com/example/sanfe/digitalcampus/activities/MenuActivity.java
+++ app/src/main/java/com/example/sanfe/digitalcampus/activities/MenuActivity.java
@@ -1,8 +1,15 @@
 package com.example.sanfe.digitalcampus.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -11,6 +18,7 @@ import com.example.sanfe.digitalcampus.R;
 import com.example.sanfe.digitalcampus.adapters.GridViewImageAdapter;
 
 public class MenuActivity  extends AppCompatActivity {
+    final static String APP_NAME = "Digital Campus";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,7 @@ public class MenuActivity  extends AppCompatActivity {
 
         Integer [] images = {R.drawable.ic_book_black_48dp, R.drawable.ic_account_box_black_48dp,
                 R.drawable.ic_description_black_48dp, R.drawable.ic_cancel_black_48dp};
-        String [] descriptions = {"  Gestionar\nAssignatura", " Gestionar Alumnado", " Examenes", "     Salir"};
+        String [] descriptions = {"  Gestionar\nAsignaturas", " Gestionar Alumnado", " Examenes", "     Salir"};
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new GridViewImageAdapter(this, images, descriptions));
@@ -40,4 +48,61 @@ public class MenuActivity  extends AppCompatActivity {
             }
         });
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.app_icon);
+        Bitmap new_icon = resizeBitmapImageFn(icon, 72);
+        Drawable d = new BitmapDrawable(getResources(), new_icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(d);
+
+        getSupportActionBar().setTitle("  " + APP_NAME);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.create_student:
+                return true;
+            case R.id.create_subject:
+                return true;
+            case R.id.new_exam:
+                return true;
+            case R.id.exit:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private Bitmap resizeBitmapImageFn(Bitmap bmpSource, int maxResolution){
+        int iWidth = bmpSource.getWidth();
+        int iHeight = bmpSource.getHeight();
+        int newWidth = iWidth ;
+        int newHeight = iHeight ;
+        float rate = 0.0f;
+
+        if (iWidth > iHeight ) {
+            if (maxResolution < iWidth ) {
+                rate = maxResolution / (float) iWidth ;
+                newHeight = (int) (iHeight * rate);
+                newWidth = maxResolution;
+            }
+        }else {
+            if (maxResolution < iHeight ) {
+                rate = maxResolution / (float) iHeight ;
+                newWidth = (int) (iWidth * rate);
+                newHeight = maxResolution;
+            }
+        }
+        return Bitmap.createScaledBitmap(bmpSource, newWidth, newHeight, true);
+    }
+
 }
